50fe9f50e04ad76cf10540595a07c81e48bcc9ab
Astrid-OC <astridtournant@gmail.com>
2024-09-05 11:25:53 +0200

    suppression du code mort dans les crud

--- src/Controller/Admin/DateHoraireCrudController.php
+++ src/Controller/Admin/DateHoraireCrudController.php
@@ -4,11 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\DateHoraire;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class DateHoraireCrudController extends AbstractCrudController
 {
