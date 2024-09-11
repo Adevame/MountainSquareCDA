086f40375290dd637bb82748fbfbb70eb430b1b3
quentin czarnynoga <czarnynoga.quentins@gmail.com>
2024-09-11 11:01:57 +0200

    modal + js

--- public/js/modalPerformer.js
+++ public/js/modalPerformer.js
@@ -1,32 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
     let currentOpenModal = null;
-
+    let overlay = document.getElementById('modalOverlay');
+    
     document.querySelectorAll('[id^="modalOn_"]').forEach(btn => {
         btn.addEventListener("click", function() {
             let passageId = this.id.split("_")[1]; 
-            let modalMockup = document.getElementById(`modalMockup_${passageId}`);
-            let overlay = document.getElementById('modalOverlay');
-            
-            overlay.style.display = "block";
-            
-            if (currentOpenModal && currentOpenModal !== modalMockup) {
+            let modalMockup = document.getElementById(`modalMockup_${passageId}`);                        
+            overlay.style.display = "block";     
+            if (currentOpenModal) {
                 currentOpenModal.style.display = "none";
-            }
-            
-            modalMockup.style.display = "grid";
-            
+            }         
+            modalMockup.style.display = "grid";          
             currentOpenModal = modalMockup;
         });
     });
 
-    document.getElementById('modalOff').addEventListener("click", function() {
-        let overlay = document.getElementById('modalOverlay');
-        
+    document.getElementById('modalOff').addEventListener("click", function() { 
         if (currentOpenModal) {
             currentOpenModal.style.display = "none";
             currentOpenModal = null;
-        }
-        
-        overlay.style.display = "none";
+        }               
+        overlay.style.display = "none";        
     });
 });
